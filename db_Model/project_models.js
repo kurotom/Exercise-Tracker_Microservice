b1cb17826a0a5b937e05136a7a17e31ab570976a
kurotom <55354389+kurotom@users.noreply.github.com>
2022-11-25 16:30:26 -0300

    api user-exercise log

--- db_Model/project_models.js
+++ db_Model/project_models.js
@@ -5,7 +5,7 @@ let { UserSchema, ExerciseSchema } = require('../db_Schemas/project_schemas.js')
 let mongoose = require('mongoose');
 
 
-const UserModel = mongoose.model('Users', UserSchema)
-const ExersiceModel = mongoose.model('Exersices', ExerciseSchema);
+const UserModel = mongoose.model('User', UserSchema)
+const ExersiceModel = mongoose.model('Exersice', ExerciseSchema);
 
 module.exports = { UserModel, ExersiceModel };
